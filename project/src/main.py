2e8f4083687acf49a96de78d20512e6c42f991d0
Sayak Mukherjee <sayakmukherjee2010@gmail.com>
2022-11-01 15:32:08 +0100

    minor changes

--- project/src/main.py
+++ project/src/main.py
@@ -14,14 +14,47 @@ from ignite.metrics import PSNR, SSIM
 from ignite.engine import Engine
 from torch.utils.data import Dataset
 
-def train(config: Config, dataset: Dataset, generator: ContextEncoder, device: str):
+def save_model(config: Config, generator: ContextEncoder, discriminator: Discriminator, name: str):
+
+    if not os.path.isdir(config.local_vars['model_path']):
+        os.mkdir(config.local_vars['model_path'])
+
+    generator_dict = generator.net.state_dict()
+    discriminator_dict = discriminator.ae_net.state_dict()
+
+    export_path = os.path.join(config.local_vars['model_path'], name + '.tar')
+
+    torch.save({'generator_dict': generator_dict,
+                'discriminator_dict': discriminator_dict}, export_path)
+
+def load_model(config: Config, generator: ContextEncoder, discriminator: Discriminator, name: str):
+
+    import_path = os.path.join(config.local_vars['model_path'], name + '.tar')
+
+    model_dict = torch.load(import_path)
+
+    generator.load_state_dict(model_dict['generator_dict'])
+    discriminator.load_state_dict(model_dict['discriminator_dict'])
+
+    return generator, discriminator
+
+def init_weights(model):
+
+    classname = model.__class__.__name__
+
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(model.weight.data, 0.0, 0.02)
+
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(model.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(model.bias.data, 0.0)
+
+def train(config: Config, dataset: Dataset, generator: ContextEncoder, discriminator: Discriminator, device: str):
 
     logger = logging.getLogger()
 
     # Disable info logs for ignite engine
     logging.getLogger("ignite.engine.engine.Engine").setLevel(logging.WARNING)
-
-    discriminator = Discriminator().to(device)
 
     # Initialising model weights
     generator.apply(init_weights)
@@ -34,12 +67,12 @@ def train(config: Config, dataset: Dataset, generator: ContextEncoder, device: s
     pixel_loss = torch.nn.L1Loss()
 
     # Optimizers
-    optimizer_G = torch.optim.Adam(generator.parameters(), 
-                                   lr = config.local_vars['learning_rate'], 
+    optimizer_G = torch.optim.Adam(generator.parameters(),
+                                   lr = config.local_vars['learning_rate'],
                                    betas = (config.local_vars['b1'], config.local_vars['b2']))
 
-    optimizer_D = torch.optim.Adam(discriminator.parameters(), 
-                                   lr = config.local_vars['learning_rate'], 
+    optimizer_D = torch.optim.Adam(discriminator.parameters(),
+                                   lr = config.local_vars['learning_rate'],
                                    betas = (config.local_vars['b1'], config.local_vars['b2']))
 
     # Get data loaders
@@ -66,7 +99,7 @@ def train(config: Config, dataset: Dataset, generator: ContextEncoder, device: s
     logger.info('Start training...')
     for epoch in range(config.local_vars['epochs']):
         for i, (images, masked_images, masked_sections) in enumerate(train_loader):
-            
+
             # True and fake labels
             valid_labels = Variable(Tensor(images.shape[0], *out_size).fill_(1.0), requires_grad=False)
             fake_labels = Variable(Tensor(images.shape[0], *out_size).fill_(0.0), requires_grad=False)
@@ -109,7 +142,7 @@ def train(config: Config, dataset: Dataset, generator: ContextEncoder, device: s
                 "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G adv: %f, pixel: %f]"
                 % (epoch, config.local_vars['epochs'], i, len(train_loader), d_loss.item(), gen_adv.item(), gen_pixel.item())
             )
-        
+
         # Validate training
         if epoch % config.local_vars['val_interval'] == 0:
 
@@ -130,17 +163,17 @@ def train(config: Config, dataset: Dataset, generator: ContextEncoder, device: s
 
                 generated_images = masked_images.clone()
 
-                generated_images[:, :, 
-                                 topLeftLoc : topLeftLoc + config.local_vars['mask_size'], 
+                generated_images[:, :,
+                                 topLeftLoc : topLeftLoc + config.local_vars['mask_size'],
                                  topLeftLoc : topLeftLoc + config.local_vars['mask_size']] = gen_parts
-                
+
 
                 state = default_evaluator.run([[generated_images, images]])
 
-                psnr_loss += state.metrics['psnr'] 
-                ssim_loss += state.metrics['ssim'] 
+                psnr_loss += state.metrics['psnr']
+                ssim_loss += state.metrics['ssim']
                 total += images.shape[0]
-        
+
             logger.info(
                 "[Validation] [PSNR loss: %f] [SSIM loss: %f]"
                 % (psnr_loss/total, ssim_loss/total )
@@ -192,34 +225,23 @@ def test(config: Config, dataset: Dataset, generator: ContextEncoder, device: st
 
         generated_images = masked_images.clone()
 
-        generated_images[:, :, 
-                         topLeftLoc : topLeftLoc + config.local_vars['mask_size'], 
+        generated_images[:, :,
+                         topLeftLoc : topLeftLoc + config.local_vars['mask_size'],
                          topLeftLoc : topLeftLoc + config.local_vars['mask_size']] = gen_parts
-        
+
 
         state = default_evaluator.run([[generated_images, images]])
 
-        psnr_loss += state.metrics['psnr'] 
-        ssim_loss += state.metrics['ssim'] 
+        psnr_loss += state.metrics['psnr']
+        ssim_loss += state.metrics['ssim']
         total += images.shape[0]
-    
+
     logger.info(
         "[Test] [PSNR loss: %f] [SSIM loss: %f]"
         % (psnr_loss/total, ssim_loss/total )
     )
 
     logger.info('Completed testing...')
-
-def init_weights(model):
-
-    classname = model.__class__.__name__
-
-    if classname.find("Conv") != -1:
-        torch.nn.init.normal_(model.weight.data, 0.0, 0.02)
-
-    elif classname.find("BatchNorm2d") != -1:
-        torch.nn.init.normal_(model.weight.data, 1.0, 0.02)
-        torch.nn.init.constant_(model.bias.data, 0.0)
 
 def main():
 
@@ -232,7 +254,7 @@ def main():
         os.mkdir(config.local_vars['log_path'])
 
     # Setup logger
-    logging.basicConfig(level=logging.INFO, 
+    logging.basicConfig(level=logging.INFO,
                         filename=config.local_vars['log_path'] + '/log.txt',
                         filemode='w',
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -241,7 +263,7 @@ def main():
 
     logger.info('Log file is %s.' % (config.local_vars['log_path'] + '/log.txt'))
     logger.info('Data path is %s.' % config.local_vars['data_path'])
-    
+
     # Set seed
     if config.local_vars['seed'] != -1:
 
@@ -259,18 +281,33 @@ def main():
 
     logger.info('Computation device: %s' % device)
 
-    dataset = ParisStreetViewDataset(root = config.local_vars['data_path'], 
-                                     image_size = config.local_vars['image_size'], 
-                                     mask_size = 64)
+    if config.local_vars['dataset'] == "parisStreetView":
+        dataset = ParisStreetViewDataset(root = config.local_vars['data_path'],
+                                        image_size = config.local_vars['image_size'],
+                                        mask_size = 64)
+
+    elif config.local_vars['dataset'] == "celeba":
+        dataset = ParisStreetViewDataset(root = config.local_vars['data_path'],
+                                        image_size = config.local_vars['image_size'],
+                                        mask_size = 64)
+    else:
+        logger.info('Unknown dataset')
+        return
+
     logger.info('Configured dataset')
 
-    generator_model = ContextEncoder().to(device)
+    # Generator and discriminator models
+    generator = ContextEncoder().to(device)
+    discriminator = Discriminator().to(device)
 
-    # train(config, dataset, generator_model, device)
+    train(config, dataset, generator, discriminator, device)
 
-    # test(config, dataset, generator_model, device)
+    test(config, dataset, generator, device)
 
-    visualize_samples(config, dataset, generator_model, device, 'Results')
+    visualize_samples(config, dataset, generator, device, 'Results_' + config.local_vars['dataset'])
+
+    # Save trained models
+    save_model(config, generator, discriminator, 'model_' + config.local_vars['dataset'])
 
 if __name__ == '__main__':
     main()
